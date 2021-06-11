2693b15a4018aea081c691cb9deaf28915c1d326
kamp-frontend <zeryasoft@gmail.com>
2021-06-11 15:19:59 +0300

    11.06.2021

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ProductList from "../pages/ProductList";
 import Categories from "./Categories";
-import Navi from "./Navi";
 import { Grid } from "semantic-ui-react";
+import { Route } from "react-router";
+import ProductDetail from "../pages/ProductDetail";
+import CartDetail from "../pages/CartDetail";
 
 export default function Dashboard() {
   return (
@@ -13,7 +15,11 @@ export default function Dashboard() {
             <Categories />
           </Grid.Column>
           <Grid.Column width={12}>
-            <ProductList />
+            <Route exact path="/" component={ProductList}/>            
+            <Route exact path="/products" component={ProductList}/>        
+            <Route exact path="/products/:id" component={ProductDetail}/>
+              
+            <Route exact path="/cart" component={CartDetail}/>
           </Grid.Column>
         </Grid.Row>
       </Grid>
